5522bc3c9638803d2b51fd1d6e9b89b7e8d1ae58
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2022-02-03 19:28:05 -0700

    Fix peoms directory bug

--- scripts/newPoem.sh
+++ scripts/newPoem.sh
@@ -1,12 +1,14 @@
 # a shell script to make new poems in the ../poems directory
 
+poemsDir="../poems"
+
 echo Title
 read title
 
 echo Date
 read date
 
-if [ -d ../poems/$date ]; then
+if [ -d $poemsDir/$date ]; then
   echo $date already exists, exiting...
   exit 1
 fi
@@ -17,8 +19,8 @@ read p1
 echo Poem 2
 read p2
 
-mkdir ./$date
+mkdir $poemsDir/$date
 
-echo ---\\ntitle: $title\\nauthor: 放歌江海山阙\\ndate: $date\\npoem: \|\\n\ \ $p1\\n\\n\ \ $p2\\n---\\n > ../poems/$date/index.md
+echo ---\\ntitle: $title\\nauthor: 放歌江海山阙\\ndate: $date\\npoem: \|\\n\ \ $p1\\n\\n\ \ $p2\\n---\\n > $poemsDir/$date/index.md
 
-open ../poems/$date/index.md
+open $poemsDir/$date/index.md
